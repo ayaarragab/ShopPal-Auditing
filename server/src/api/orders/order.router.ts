9746b7ca017ee4a7832c4b1ada531ce1cb73a60c
ayaarragab <ayarsk4343@gmail.com>
2026-08-20 19:28:02 +0300

    fix(Security): Added authrization layer in Product Stock Route

--- server/src/api/orders/order.router.ts
+++ server/src/api/orders/order.router.ts
@@ -3,60 +3,61 @@ import { check, checkSchema } from 'express-validator';
 import { OrderController } from '@/api/orders/order.controller';
 import { validationMiddleware, authorizationMiddleware, tryCatchMiddleware } from '@/middlewares';
 import {
-    getOrdersSchema,
-    createOrderForGuestUserSchema,
-    createOrderForAuthenticatedUserSchema,
-    updateOrderSchema,
-    deleteOrderSchema,
-    updateProductsStocksSchema,
+	getOrdersSchema,
+	createOrderForGuestUserSchema,
+	createOrderForAuthenticatedUserSchema,
+	updateOrderSchema,
+	deleteOrderSchema,
+	updateProductsStocksSchema,
 } from '@/api/orders/order.validator';
 
 const router = Router();
 
 router.get(
-    '/:user_id',
-    authorizationMiddleware,
-    checkSchema(getOrdersSchema),
-    validationMiddleware,
-    tryCatchMiddleware(OrderController.getOrders)
+	'/:user_id',
+	authorizationMiddleware,
+	checkSchema(getOrdersSchema),
+	validationMiddleware,
+	tryCatchMiddleware(OrderController.getOrders)
 );
 
 router.post(
-    '/',
-    checkSchema(createOrderForGuestUserSchema),
-    validationMiddleware,
-    tryCatchMiddleware(OrderController.createOrderForGuestUser)
+	'/',
+	checkSchema(createOrderForGuestUserSchema),
+	validationMiddleware,
+	tryCatchMiddleware(OrderController.createOrderForGuestUser)
 );
 
 router.post(
-    '/:user_id',
-    authorizationMiddleware,
-    checkSchema(createOrderForAuthenticatedUserSchema),
-    validationMiddleware,
-    tryCatchMiddleware(OrderController.createOrderForAuthenticatedUser)
+	'/:user_id',
+	authorizationMiddleware,
+	checkSchema(createOrderForAuthenticatedUserSchema),
+	validationMiddleware,
+	tryCatchMiddleware(OrderController.createOrderForAuthenticatedUser)
 );
 
 router.patch(
-    '/:order_id',
-    authorizationMiddleware,
-    checkSchema(updateOrderSchema),
-    validationMiddleware,
-    tryCatchMiddleware(OrderController.updateOrder)
+	'/:order_id',
+	authorizationMiddleware,
+	checkSchema(updateOrderSchema),
+	validationMiddleware,
+	tryCatchMiddleware(OrderController.updateOrder)
 );
 
 router.patch(
-    '/',
-    checkSchema(updateProductsStocksSchema),
-    validationMiddleware,
-    tryCatchMiddleware(OrderController.updateProductsStocks)
+	'/',
+	authorizationMiddleware,
+	checkSchema(updateProductsStocksSchema),
+	validationMiddleware,
+	tryCatchMiddleware(OrderController.updateProductsStocks)
 );
 
 router.delete(
-    '/:order_id',
-    authorizationMiddleware,
-    checkSchema(deleteOrderSchema),
-    validationMiddleware,
-    tryCatchMiddleware(OrderController.deleteOrder)
+	'/:order_id',
+	authorizationMiddleware,
+	checkSchema(deleteOrderSchema),
+	validationMiddleware,
+	tryCatchMiddleware(OrderController.deleteOrder)
 );
 
 export default router;
