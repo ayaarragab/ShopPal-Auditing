d1a61580a1eb09aad7b4333447e6d2730c52f031
ayaarragab <ayarsk4343@gmail.com>
2026-08-20 19:33:28 +0300

    style: format product router code for improved readability

--- server/src/api/products/product.router.ts
+++ server/src/api/products/product.router.ts
@@ -6,37 +6,47 @@ import { checkSchema } from 'express-validator';
 import ProductSchemaValidator from './product.validator';
 const router = Router();
 
-router.get('/', 
-    checkSchema(ProductSchemaValidator.getManyProductSchema),
-    validationMiddleware,
-    tryCatchMiddleware(ProductController.getManyProducts));
+router.get(
+	'/',
+	checkSchema(ProductSchemaValidator.getManyProductSchema),
+	validationMiddleware,
+	tryCatchMiddleware(ProductController.getManyProducts)
+);
 
 // NOTE: create product route will not be implemented right now in the frontend
 // TODO: requires authorization based on current logged user role
-router.post('/', 
-    authorizationMiddleware,
-    checkSchema(ProductSchemaValidator.createProductSchema),
-    validationMiddleware,    
-    tryCatchMiddleware(ProductController.createProduct));
+router.post(
+	'/',
+	authorizationMiddleware,
+	checkSchema(ProductSchemaValidator.createProductSchema),
+	validationMiddleware,
+	tryCatchMiddleware(ProductController.createProduct)
+);
 
-router.get('/:product_id', 
-    checkSchema(ProductSchemaValidator.getOneProductSchema),
-    validationMiddleware,
-    tryCatchMiddleware(ProductController.getOneProduct));
+router.get(
+	'/:product_id',
+	checkSchema(ProductSchemaValidator.getOneProductSchema),
+	validationMiddleware,
+	tryCatchMiddleware(ProductController.getOneProduct)
+);
 
 // NOTE: update product route will not be implemented right now in the frontend
 // TODO: requires authorization based on current logged user role
-router.patch('/:product_id', 
-    authorizationMiddleware,
-    checkSchema(ProductSchemaValidator.updateProductSchema),
-    validationMiddleware,
-    tryCatchMiddleware(ProductController.updateProduct));
+router.patch(
+	'/:product_id',
+	authorizationMiddleware,
+	checkSchema(ProductSchemaValidator.updateProductSchema),
+	validationMiddleware,
+	tryCatchMiddleware(ProductController.updateProduct)
+);
 
 // NOTE: delete product route will not be implemented right now in the frontend
-// TODO: requires authorization based on current logged user role 
-router.delete('/:product_id', 
-    checkSchema(ProductSchemaValidator.deleteProductSchema),
-    validationMiddleware,
-    tryCatchMiddleware(ProductController.deleteProduct));
+// TODO: requires authorization based on current logged user role
+router.delete(
+	'/:product_id',
+	checkSchema(ProductSchemaValidator.deleteProductSchema),
+	validationMiddleware,
+	tryCatchMiddleware(ProductController.deleteProduct)
+);
 
 export default router;
