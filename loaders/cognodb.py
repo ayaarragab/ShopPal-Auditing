1e5cb2d09730354d03cdad1c8fad5d7e678df2ff
ayaarragab <ayarsk4343@gmail.com>
2026-08-21 04:23:01 +0300

    feat: first DB setup

---
+++ loaders/cognodb.py
@@ -0,0 +1,17 @@
+from neo4j import GraphDatabase
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+driver = GraphDatabase.driver(
+    os.environ["CONGODB_CONNECTION_URI"],
+    auth=(
+        os.environ["CONGODB_USERNAME"],
+        os.environ["CONGODB_PASSWORD"],
+    ),
+)
+
+driver.verify_connectivity()
+
+print("Connected successfully")
